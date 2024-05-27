4d5fa1907b63458253e1018cbcdb2bb39d82acb6
Jorge Arnal <jorge.arnal08@gmail.com>
2024-05-26 23:47:16 -0300

    Agrego el fetch en Abrigos

---
+++ static/js/abrigosFetch.js
@@ -0,0 +1,34 @@
+// Obtener el valor de "categoria" en la url del menú
+const urlParams = new URLSearchParams(window.location.search);
+const categoria = urlParams.get('categoria');
+
+// Llamada a la api                                                        acá toma el valor que tiene "categoria" en la url del nav
+const respuesta = fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${categoria}&limit=3#json`);
+
+respuesta
+    .then(res => res.json())
+    .then(response => renderAbrigos(response))
+    .catch(err => console.error(err));
+
+
+// Función que crea la card de abrigos
+function renderAbrigos(response) {
+    const abrigos = response.results;
+    let card = '';
+    for (let abrigo of abrigos) {
+        card += `
+        <div class="card">
+            <img src="${abrigo.thumbnail}"
+                class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${abrigo.title}</h5>
+                <p class="card-text">
+                </p>
+                <a href="abrigo2.html" class="btn btn-primary">Ver mas</a>
+            </div>
+        </div>
+        `
+    }
+
+    document.querySelector('.cards').innerHTML = card;
+}
