d87ce8fae28e2a6a7e33d67e3898c5b5e094ef5c
Merlin Medina <merlinangel.medina@gmail.com>
2024-05-27 19:25:25 -0300

    se crea articulosIndex y articulosFetch para api

--- static/js/articulosFetch.js
+++ static/js/articulosFetch.js
@@ -2,6 +2,10 @@
 const urlParams = new URLSearchParams(window.location.search);
 const categoria = urlParams.get('categoria');
 
+// Cartel de cargando... hasta que regresa el llamado de la api
+document.querySelector('.cards').innerHTML = `
+<h1 class="text-center fs-1 fw-bold text-white bg-dark rounded-pill">CARGANDO...</h1>`;
+
 // Llamada a la api                                                        acá toma el valor que tiene "categoria" en la url del nav
 const respuesta = fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${categoria}&limit=3#json`);
 
